97125ab0fb24ee328722fe5d605c33318a9ef31d
xlsj <xlsj@fresnou.org>
2023-03-24 10:08:15 -0700

    got work done

---
+++ return1.js
@@ -0,0 +1,15 @@
+const readline= require('readline-sync');
+
+//1
+let name = (readline.question("Give me your first name: "));
+let last = (readline.question("Give me your last name : "));
+let dad = (` Greetings ${last}, ${name}`);
+console.log(dad);
+
+//2
+let num = (readline.question("Give me your first name: "));
+let num1 = (readline.question("Give me your last name : "));
+let mom = (` Greetings ${last}, ${name}`);
+console.log(dad);
+
+
